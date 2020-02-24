3e8e810ca97e7279f6369304d0ad9024f609b4cd
har4enko <pan4enko48@gmail.com>
2020-02-24 14:03:28 +0200

    Changed push to spred

--- main.js
+++ main.js
@@ -1,50 +1,56 @@
-const students = ["Саша", "Игорь", "Лена", "Ира", "Алексей", "Светлана"];
-const studentsGirl = ["Лена", "Ира", "Светлана"];
-const studentsBoy = ["Саша", "Игорь", "Алексей"];
-const themes = ["Дифференциальные уравнения", "Теория автоматов", "Алгоритмы и структуры данных"];
+const students = ['Саша', 'Игорь', 'Лена', 'Ира', 'Алексей', 'Светлана'];
+const studentsGirl = ['Лена', 'Ира', 'Светлана'];
+const studentsBoy = ['Саша', 'Игорь', 'Алексей'];
+const themes = [
+  'Дифференциальные уравнения',
+  'Теория автоматов',
+  'Алгоритмы и структуры данных'
+];
 const marks = [4, 5, 5, 3, 4, 5];
 
-
 function getPairs() {
-    let result = [];
-    for (let i = 0; i < studentsGirl.length; i++) {
-        result.push([studentsGirl[i],studentsBoy[i]]);
-    }
-    return result;
+  let result = [];
+  for (let i = 0; i < studentsGirl.length; i++) {
+    result = [...result, [studentsGirl[i], studentsBoy[i]]];
+  }
+  return result;
 }
 const pairs = getPairs();
 console.log(pairs);
 
-
-
 function pairsAndThemes() {
-    let result = [];
-    for (let i = 0; i < studentsGirl.length; i++) {
-        result.push([studentsGirl[i] + " и " + studentsBoy[i], themes[i]]);
-    }
-    return result;
+  let result = [];
+  for (let i = 0; i < studentsGirl.length; i++) {
+    result = [...result, [studentsGirl[i] + ' и ' + studentsBoy[i], themes[i]]];
+  }
+  return result;
 }
 const pAt = pairsAndThemes();
 console.log(pAt);
 
-
-
 function success() {
-    let result = [];
-    for (let i = 0; i < students.length; i++) {
-        result.push([students[i],marks[i]]);
-    }
-    return result;
+  let result = [];
+  for (let i = 0; i < students.length; i++) {
+    result = [...result, [students[i], marks[i]]];
+  }
+  return result;
 }
 const s = success();
 console.log(s);
 
-function studentsThemesAndMarks () {
-    let result = [];
-    for (let i = 0; i <studentsGirl.length ; i++) {
-        result.push([studentsGirl[i] + " и " + studentsBoy[i], themes[i], Math.floor(Math.random()*5+1)]);
-    }
-    return result;
+function studentsThemesAndMarks() {
+  let result = [];
+  for (let i = 0; i < studentsGirl.length; i++) {
+    result = [
+      ...result,
+      [
+        studentsGirl[i] + ' и ' + studentsBoy[i],
+        themes[i],
+        Math.floor(Math.random() * 5 + 1)
+      ]
+    ];
+  }
+  return result;
 }
 const sTAM = studentsThemesAndMarks();
 console.log(sTAM);
